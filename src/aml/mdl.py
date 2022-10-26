08746719148e740de64a980cba4545f99056f602
farinamhz <f.hemmmatizadeh@gmail.com>
2022-10-26 19:22:38 -0400

    Quality getter function added (#5)

--- src/aml/mdl.py
+++ src/aml/mdl.py
@@ -33,17 +33,28 @@ class AbstractAspectModel:
         else: reviews_ = [s for r in reviews for s in r.sentences]  # doctype = 'sentence'
         return [[word for word in doc if word not in stop_words and len(word) > 3 and re.match('[a-zA-Z]+', word)] for doc in reviews_]
 
-    @staticmethod
-    def plot_coherence(path, cas): # dict of coherences for different naspects, e.g., {'2': [0.3, 0.5], '3': [0.3, 0.5, 0.7]}.
-        # np.mean(row wise)
-        # np.std(row wise)
 
-        # plt.plot(x, mean, '-or', label='mean')
-        # plt.xlim(start - 0.025, limit - 1 + 0.025)
-        plt.xlabel("#aspects")
-        plt.ylabel("coherence")
-        plt.legend(loc='best')
-        plt.savefig(f'{path}coherence.png')
-        plt.clf()
+    @staticmethod
+    def quality(model, metric):
+        result = {"Coherence": f'{np.mean(model.cas)}\u00B1{np.std(model.cas)}',
+                  "Perplexity": model.perplexity
+                  }
+        return result[metric]
+        # elif metric is "perplexity":
+        #     return
+
+    # @staticmethod
+    # def plot_coherence(path, cas):
+    # dict of coherences for different naspects, e.g., {'2': [0.3, 0.5], '3': [0.3, 0.5, 0.7]}.
+    #     # np.mean(row wise)
+    #     # np.std(row wise)
+    #
+    #     # plt.plot(x, mean, '-or', label='mean')
+    #     # plt.xlim(start - 0.025, limit - 1 + 0.025)
+    #     plt.xlabel("#aspects")
+    #     plt.ylabel("coherence")
+    #     plt.legend(loc='best')
+    #     plt.savefig(f'{path}coherence.png')
+    #     plt.clf()
 
 
