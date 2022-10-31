de6592c706176ffdd80ad50b71395ff4528bcc37
farinamhz <f.hemmmatizadeh@gmail.com>
2022-10-31 14:27:32 -0400

    New params added to the new architecture

--- src/params.py
+++ src/params.py
@@ -1,5 +1,6 @@
 seed = 0
 
+# aspect modeling settings
 no_extremes = {
     'no_below': 10,    # happen less than no_below number in total
     'no_above': 0.9,    # happen in no_above percent of reviews
@@ -7,9 +8,15 @@ no_extremes = {
 doctype = 'snt' # 'rvw' ==> if 'rvw': review = [[review]] else if 'snt': review = [[subreview1], [subreview2], ...]
 iter = 100
 cores = 0
-
-
+nwords = 20
+qualities = ['Coherence', 'Perplexity']
+# training settings
 train_ratio = 0.85 # 1 - train_ratio goes to test
 nfolds = 5 # on the train, nfold x-valid
+
+# evaluation settings
+metrics = ['success', 'P', 'recall', 'ndcg_cut', 'map_cut']
+
+topkstr = '1,2,5,10,100,1000'
 
 topk = 10
