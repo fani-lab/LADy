ba33c28c4f653590100de7d99fca9009ef5773fc
farinamhz <f.hemmmatizadeh@gmail.com>
2022-11-03 15:57:36 -0400

    Random baseline added for aspect modeling (#9)

--- src/main.py
+++ src/main.py
@@ -5,8 +5,14 @@ from json import JSONEncoder
 import pandas as pd
 import pytrec_eval
 
-
 import params
+
+
+from src.aml.mdl import AbstractAspectModel
+from src.aml.lda import Lda
+from src.aml.btm import Btm
+from src.aml.rnd import Rnd
+
 
 def load(input, output):
     print('\n1. Loading reviews and preprocessing ...')
@@ -47,51 +53,8 @@ def split(nsample, output):
     with open(f'{output}/splits.json', 'w') as f: json.dump(splits, f, cls=NumpyArrayEncoder, indent=1)
     return splits
 
-def train(reviews, naspects, output, aml):
-    print(f'\n2. Aspect modeling ...')
-    print('#' * 50)
-    t_s = time()
-    try:
-        print(f'2.1. Loading saved aspect model from {output} ...')
-        print("aml: ", aml)
-        if "lda" in aml:
-            # print("lda")
-            from aml.lda import GensimModel
-            am = GensimModel(reviews, naspects, params.no_extremes, output)
-            am.load()
-        elif "btm" in aml:
-            # print("btm")
-            am = ""
-    except (FileNotFoundError, EOFError) as e:
-        print(f'2.1. Loading saved aspect model failed! Training a model for {naspects} of aspects. See {output}model.train.log for training logs ...')
-        am.train(params.doctype, multiprocessing.cpu_count() if params.cores <= 0 else params.cores, params.iter, params.seed)
-    print(f'2.2. Quality of aspects ...')
-    if "lda" in aml:
-        print(f'(Coherence: {np.mean(am.cas)}\u00B1{np.std(am.cas)})')
-    elif "btm" in aml:
-        # print(f'(Coherence: {np.mean(am.coherence_)}\u00B1{np.std(am.coherence_)})')
-        pass
-    print(f'Time elapsed: {(time() - t_s)}')
-    return am
 
-def metrics(pairs, per_instance=False, metrics={'success_1,2,5,10,100', 'P_1,2,5,10,100', 'recall_1,2,5,10,100', 'ndcg_cut_1,2,5,10,100', 'map_cut_1,2,5,10,100'}):
-    qrel = dict(); run = dict()
-    print(f'3.1. Building pytrec_eval input for {len(pairs)} instances ...')
-    for i, pair in enumerate(pairs):
-        qrel['q' + str(i)] = {w: 1 for w in pair[0]}
-        # the prediction list may have duplicates
-        run['q' + str(i)] = {}
-        for j, (w, p) in enumerate(pair[1]):
-            if w not in run['q' + str(i)].keys(): run['q' + str(i)][w] = len(pair[1]) - j
-        # run['q' + str(i)] = {w: len(pair[1]) - i for i, (w, p) in enumerate(pair[1])}
-
-    print(f'3.2. Calling pytrec_eval for {metrics} ...')
-    df = pd.DataFrame.from_dict(pytrec_eval.RelevanceEvaluator(qrel, metrics).evaluate(run))
-    print(f'3.3. Averaging ...')
-    df_mean = df.mean(axis=1).to_frame('mean')
-    return df if per_instance else None, df_mean
-
-def evaluate(am, test):
+def evaluate(am, am_type, test):
     print(f'\n3. Evaluating on test set ...')
     print('#' * 50)
     pairs = []
@@ -99,12 +62,42 @@ def evaluate(am, test):
         r_aspects = [[w for a, o, s in sent for w in a] for sent in r.get_aos()]  # [['service', 'food'], ['service'], ...]
         r_ = r.hide_aspects()
         r_pred_aspects = am.infer(params.doctype, r_)
-        for i, subr_pred_aspects in enumerate(r_pred_aspects):
-            subr_pred_aspects_words = [w_p for l in [[(w, a_p * w_p) for w, w_p in am.mdl.show_topic(a, topn=100)] for a, a_p in subr_pred_aspects] for w_p in l]
-            subr_pred_aspects_words = sorted(subr_pred_aspects_words, reverse=True, key=lambda t: t[1])
-            # removing duplicate aspect words ==> handled in metrics()
-            pairs.append((r_aspects[i], subr_pred_aspects_words))
-    return metrics(pairs, per_instance=True)
+        if "btm" in am_type:
+            pass
+        elif "rnd" in am_type:
+            for i in range(len(r_aspects)):
+                pairs.append((r_aspects[i], r_pred_aspects))
+        else:  # "lda" in am_type:
+            for i, subr_pred_aspects in enumerate(r_pred_aspects):
+                subr_pred_aspects_words = [w_p for l in [[(w, a_p * w_p) for w, w_p in am.mdl.show_topic(a, topn=100)] for a, a_p in subr_pred_aspects] for w_p in l]
+                subr_pred_aspects_words = sorted(subr_pred_aspects_words, reverse=True, key=lambda t: t[1])
+                # removing duplicate aspect words ==> handled in metrics()
+                pairs.append((r_aspects[i], subr_pred_aspects_words))
+
+    metrics_set = set()
+    for m in params.metrics:
+        metrics_set.add(f'{m}_{params.topkstr}')
+    qrel = dict(); run = dict()
+    print(f'3.1. Building pytrec_eval input for {len(pairs)} instances ...')
+    for i, pair in enumerate(pairs):
+        qrel['q' + str(i)] = {w: 1 for w in pair[0]}
+        # the prediction list may have duplicates
+        run['q' + str(i)] = {}
+        if "btm" in am_type or "lda" in am_type:
+            # print(pair[1])
+            for j, (w, p) in enumerate(pair[1]):
+                if w not in run['q' + str(i)].keys(): run['q' + str(i)][w] = len(pair[1]) - j
+        else:  # rnd
+            # print(pair[1])
+            for j in range(len(pair[1])):
+                # print(pair[1][j])
+                run['q' + str(i)][pair[1][j]] = len(pair[1]) - j
+    print(f'3.2. Calling pytrec_eval for {metrics_set} ...')
+    df = pd.DataFrame.from_dict(pytrec_eval.RelevanceEvaluator(qrel, metrics_set).evaluate(run))
+    print(f'3.3. Averaging ...')
+    df_mean = df.mean(axis=1).to_frame('mean')
+    return df_mean
+
 
 def main(args):
     if not os.path.isdir(f'{args.output}/{args.naspects}'): os.makedirs(f'{args.output}/{args.naspects}')
@@ -116,11 +109,36 @@ def main(args):
     output = f'{args.output}/{args.naspects}'
     for f in splits['folds'].keys():
         output_ = f'{output}/f{f}.'
-        am = train(np.array(reviews)[splits['folds'][f]['train']].tolist(), args.naspects, output_, args.aml)
-        _, df_mean = evaluate(am, test)
+        am_type = args.aml
+        model_review = np.array(reviews)[splits['folds'][f]['train']].tolist()
+        if "btm" in am_type:
+            am = Btm(model_review, args.naspects, params.no_extremes, output)
+        elif "rnd" in am_type:
+            am = Rnd(model_review, args.naspects, params.no_extremes, output)
+        else:  # if am_type == "lda"
+            am = Lda(model_review, args.naspects, params.no_extremes, output)
+
+        # training
+        print(f'\n2. Aspect modeling ...')
+        print('#' * 50)
+        t_s = time()
+        try:
+            print(f'2.1. Loading saved aspect model from {output} ...')
+            am.load()
+        except (FileNotFoundError, EOFError) as e:
+            print(
+                f'2.1. Loading saved aspect model failed! Training a model for {args.naspects} of aspects. See {output_}model.train.log for training logs ...')
+            am.train(params.doctype, multiprocessing.cpu_count() if params.cores <= 0 else params.cores, params.iter_c,
+                     params.seed)
+        print(f'2.2. Quality of aspects ...')
+        for q in params.qualities:
+            print(f'({q}: {AbstractAspectModel.quality(am, q)})')
+        print(f'Time elapsed: {(time() - t_s)}')
+        df_mean = evaluate(am, am_type, test)
         df_mean.to_csv(f'{output_}pred.eval.mean.csv')
         fold_mean = pd.concat([fold_mean, df_mean], axis=1)
     fold_mean.mean(axis=1).to_frame('mean').to_csv(f'{output}/pred.eval.mean.csv')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='PXP Topic Modeling.')
