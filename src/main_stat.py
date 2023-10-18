304dfa03844d288ab898c65f13688300a3636711
Christine Wong <55353492+Lillliant@users.noreply.github.com>
2023-10-18 17:53:29 -0400

    Revert main_stat.py back: before stat readme

--- src/main_stat.py
+++ src/main_stat.py
@@ -1,45 +1,29 @@
 import pandas as pd
-import os
 
 from cmn.review import Review
 if __name__ == '__main__':
     datasets = [
-        ("C:/Users/tea-n_/Documents/GitHub/LADy/data/raw/semeval/2016SB5/ABSA16_Restaurants_Train_SB1_v2.xml","semeval-16-restaurant"),
-        ("C:/Users/tea-n_/Documents/GitHub/LADy/data/raw/semeval/2015SB12/ABSA15_RestaurantsTrain/ABSA-15_Restaurants_Train_Final.xml", "semeval-15-restaurant"),
-        ("C:/Users/tea-n_/Documents/GitHub/LADy/data/raw/semeval/SemEval-14/Laptop_Train_v2.xml","semeval-14-laptop"),
-        ("C:/Users/tea-n_/Documents/GitHub/LADy/data/raw/semeval/SemEval-14/Semeval-14-Restaurants_Train.xml", "semeval-14-restaurant"),
-        ("C:/Users/tea-n_/Documents/GitHub/LADy/data/raw/twitter/acl-14-short-data/train.raw","twitter"),
-        ("C:/Users/tea-n_/Documents/GitHub/LADy/data/raw/mams/train.xml", "mams")
-    ]
+        ('../output/toy.2016SB5', 'toy'),
+        ('../output/SemEval-14/Laptop', 'semeval-14-laptop'),
+        ('../output/SemEval-14/Restaurants', 'semeval-14-restaurant'),
+        ('../output/2015SB12', 'semeval-15-restaurant'),
+        ('../output/2016SB5', 'semeval-16-restaurant'),
+            ]
+    # dff = pd.DataFrame()
+    # for data, title  in datasets:
+    #     stats = Review.get_stats(f'{data}/reviews.pes_Arab.zho_Hans.deu_Latn.arb_Arab.fra_Latn.spa_Latn.pkl', data, plot=True, plot_title=None)
+    #     df = pd.DataFrame.from_dict([stats['*avg_lang_stats']])
+    #     df['nreviews'] = stats['*nreviews']
+    #     df['naspects'] = stats['*naspects']
+    #     df['ntokens'] = stats['*ntokens']
+    #     df['avg_ntokens_review'] = stats['*avg_ntokens_review']
+    #     df['avg_naspects_review'] = stats['*avg_naspects_review']
+    #     df['dataset'] = data
+    #     df.set_index('dataset', inplace=True)
+    #     dff = pd.concat([dff, df])
+    # dff.to_csv('../output/semeval+/stats.csv', index=True)
 
-    dff = pd.DataFrame()
-    for data, output in datasets:
-        print(data)
-        print(output)
-        print("==================")
-        if "semeval" in data.lower():
-            from cmn.semeval import SemEvalReview
-            reviews = SemEvalReview.load(data)
-        elif "twitter" in data.lower():
-            from cmn.twitter import TwitterReview
-            reviews = TwitterReview.load(data)
-        else:
-            from cmn.mams import MAMSReview
-            reviews = MAMSReview.load(data)
-        
-        if not os.path.isdir(output): os.makedirs(output)
-        pd.to_pickle(reviews, f'{output}/reviews.pkl')
+    for data, title in datasets:
+        Review.plot_semsim_dist(f'{data}/reviews.pes_Arab.zho_Hans.deu_Latn.arb_Arab.fra_Latn.spa_Latn.pkl', f'{data}/reviews.pes_Arab.zho_Hans.deu_Latn.arb_Arab.fra_Latn.spa_Latn.hist.pdf', plot_title=title)
 
-        stats = Review.get_stats(f'{output}/reviews.pkl', output, plot=False, plot_title=None)
-        df = pd.DataFrame.from_dict([stats['*avg_lang_stats']])
-        df['nreviews'] = stats['*nreviews']
-        df['naspects'] = stats['*naspects']
-        df['ntokens'] = stats['*ntokens']
-        df['avg_ntokens_review'] = stats['*avg_ntokens_review']
-        df['avg_naspects_review'] = stats['*avg_naspects_review']
-        df['dataset'] = data
-        df.set_index('dataset', inplace=True)
-        dff = pd.concat([dff, df])
-        print(dff)
-        print("==================")
-        dff.to_csv(f'{output}stats.csv', index=True)
+
