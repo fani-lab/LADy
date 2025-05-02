cc7b7d8b05a4bf519a4820c61a2e841536d3d97f
Karanveer Singh Sidhu <>
2025-05-02 15:19:36 -0400

    (#72) Made Changes to Twitter.py file, added implicit and explicit tags.

--- src/cmn/twitter.py
+++ src/cmn/twitter.py
@@ -5,11 +5,12 @@ class TwitterReview(Review):
     def __init__(self, id, sentences, time, author, aos):
         super().__init__(self, id, sentences, time, author, aos)
 
-    def load(path):
-        return TwitterReview._loader(path)
+    @staticmethod
+    def load(path, explicit=True, implicit=False):
+        return TwitterReview._loader(path, explicit, implicit)
 
     @staticmethod
-    def _loader(path):
+    def _loader(path, explicit, implicit):
         reviews_list = []
         r_id = 0
         with open(path, 'r', encoding="utf8") as f:
@@ -17,11 +18,21 @@ class TwitterReview(Review):
 
         for idx in range(0, len(lines), 3):
             text, aspect, sentiment = lines[idx:idx + 3]
-            current_text = text.replace('$T$', aspect)
-            aos_list_list = [[(list(range(text.split().index('$T$'),
+            
+            if not implicit and aspect == 'NULL': continue
+            if not explicit and aspect != 'NULL': continue
+            
+            current_text = text.replace('$T$', aspect if aspect != 'NULL' else '')
+            implicit_arr = [aspect == 'NULL']
+            
+            if aspect != 'NULL':
+                aos_list_list = [[(list(range(text.split().index('$T$'),
                                           text.split().index('$T$') + len(aspect.split()))), [], sentiment)]]
+            else:
+                aos_list_list = [[( [None], [], sentiment)]]
+                
             reviews_list.append(
                 Review(id=r_id, sentences=[[str(t).lower() for t in current_text.split()]], time=None,
-                       author=None, aos=aos_list_list, lempos=""))
+                       author=None, aos=aos_list_list, lempos="", implicit=implicit_arr))
             r_id += 1
         return reviews_list
